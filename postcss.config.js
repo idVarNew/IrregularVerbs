3d60bac89de5fdd63b01d68502d4870cee6a746e
Ewelina Brus <idvarnew@gmail.com>
2018-03-23 02:01:01 +0100

    File was modified

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,3 @@
-module.exports = {
-    parser: 'sugarss',
-    plugins: {
-      'postcss-import': {},
-      'postcss-cssnext': {},
-      'cssnano': {}
-    }
-  }
-
   module.exports = {
     plugins: [
       require('autoprefixer')
